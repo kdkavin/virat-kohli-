fa59882600a38c021669c167a2a5a7fac5ff1d1c
kdkavin <kavin.v.qa@gmail.com>
2024-07-19 09:04:27 +0530

    first commit

---
+++ src/kovilpatti/Openheimer.java
@@ -0,0 +1,13 @@
+package kovilpatti;
+
+public class Openheimer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver","D:\\chrome-win64 (2)\\chrome-win64");
+		
+		
+	}
+
+}
